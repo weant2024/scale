800250c0045d525bfe4612e8057e277f7a87feb1
WEANT <weant2024@gmail.com>
2024-06-24 17:16:06 -0300

    Delete config directory

--- config/js/script_date_utils.js
+++
@@ -1,7 +0,0 @@
-// Função para formatar a data
-function formatDate(date) {
-    const day = date.getDate().toString().padStart(2, '0');
-    const month = (date.getMonth() + 1).toString().padStart(2, '0');
-    const year = date.getFullYear();
-    return `${day}/${month}/${year}`;
-}
--- config/js/script_remove_employee_day_index.js
+++
@@ -1,9 +0,0 @@
-function removeEmployee(day, index) {
-    if (employeesSchedule[day] && employeesSchedule[day][index]) {
-        employeesSchedule[day].splice(index, 1);
-        saveSchedule();
-        alert('Funcionário removido com sucesso da escala.');
-    } else {
-        alert('Funcionário não encontrado na escala.');
-    }
-}
